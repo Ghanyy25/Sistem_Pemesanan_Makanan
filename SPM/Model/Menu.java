aceda843f559b55220c810f7981765b9272bc742
SYECHYUSUF <mochsyechyusufm@gmail.com>
2025-05-14 15:53:44 +0800

    Menambahkan Menu dan Order

--- SPM/Model/Menu.java
+++ SPM/Model/Menu.java
@@ -2,7 +2,43 @@ package Model;
 
 
 public class Menu {
-    
+    private static int nextId = 1;
+    private int id;
+    private String nama;
+    private double harga;
+
+    public Menu(String nama, double harga) {
+        this.id = nextId++; 
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getNama() {
+        return nama;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    public double getHarga() {
+        return harga;
+    }
+
+    @Override
+    public String toString() {
+        return id + ". " + nama + " - Rp" + harga;
+    }
 }
+
+
+
+
 
 
--- SPM/Model/Order.java
+++ SPM/Model/Order.java
@@ -3,5 +3,36 @@ package Model;
 import java.util.*;
 
 public class Order {
-    
+    private String namaCustomer;
+    private List<Menu> items;
+    private double totalHarga;
+
+    public Order(String namaCustomer, List<Menu> items) {
+        this.namaCustomer = namaCustomer;
+        this.items = new ArrayList<>(items);
+        hitungTotalHarga();
+    }
+
+public double getTotalHarga() {
+    return totalHarga;
 }
+
+    public void hitungTotalHarga() {
+        totalHarga = 0;
+        for (Menu item : items) {
+            totalHarga += item.getHarga();
+        }
+    }
+
+    public void printStruk() {
+        System.out.println("\n--- Struk Pemesanan ---");
+        System.out.println("Customer: " + namaCustomer);
+        for (Menu item : items) {
+            System.out.println("- " + item.getNama() + " (Rp" + item.getHarga() + ")");
+        }
+        System.out.println("Total Harga: Rp" + totalHarga);
+        System.out.println("------------------------");
+    }
+}
+
+
